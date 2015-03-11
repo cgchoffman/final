dc6ee6d25e8cd7163976f21778254a1b87b13a37
Carey Hoffman <cgchoffman@gmail.com>
2015-03-10 21:32:19 -0700

    Fixed map load issues.  Wasn't using location because it didn't even have it yet.

--- scripts/biere.js
+++ scripts/biere.js
@@ -1,7 +1,7 @@
 var $map_out = $("#map_out");  // The container for the map
 var map;  //  The Actual map it's self
 var mapStyles = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}]
-var users_location;
+var user_position;
 var default_location = new google.maps.LatLng(49.281947, -123.121167);
 var $loader = $("#loader");
 var $spinner = $("#load_spinner");
@@ -27,57 +27,65 @@ var opts = {
 var target = document.getElementById('load_spinner');
 var spinner = new Spinner(opts).spin(target);
 
+console.log("Lets get started!  Where's the craft at??")
 console.log("Getting user location.");
-getLocation()
-console.log("Waiting 5 seconds for user location.");
-setTimeout(function(){
-  init()
-  }, 4000);
-
-function init() {
-  try{
-	console.log("Waited 5 seconds, now i'm making a map!");
-	console.log("\tUser Location: " + users_location);
-	console.log("\tDefault User Location: " + default_location);
-	// If user location didn't get set use a default.
-	if (!users_location) {
-	  console.log("No user location, using default.");
-		var users_location = default_location;
-	}
-	mapOptions = {
-		zoom: 15,
-		center: users_location,
-        styles: mapStyles
-	}
-	
-	showHideLoader();
-	//wait two seconds for showHideLoader to complete
-	setTimeout( function(){ showHideMap() }, 2000);
-	
-  } catch (e) {
-	console.log("Initializing map failed: " + e);
-  }
-
-}
-
-function getLocation() {
-	if ("geolocation" in navigator) {
-		navigator.geolocation.getCurrentPosition(function(position){setPosition(position)},
-                                                 function(){users_location = default_location});
-	} else { 
-	   console.log("Geolocation is not supported by this browser.  Use default.");
-	}
-}
-
-function setPosition(position) {
-	console.log("setPosition:");
-	console.log("\tUsers Lat: " + ((position && position.coords) ? position.coords.latitude : null));
-	console.log("\tUsers Long: " + ((position && position.coords) ? position.coords.longitude : null));
-	users_location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);	
-}
+getLocation() // This allows me to load the map only once the users location hase been given.
 
 /**
- * This actually only SHOWS the Map.  When it gets more complicated it can show and hide
+ * Request user location, when received, called setPosition to actually set it
+ */
+function getLocation() {
+    var geo_options = {
+        enableHighAccuracy: true, 
+        maximumAge        : 30000, 
+        timeout           : 27000
+    };
+	if ("geolocation" in navigator) {
+		navigator.geolocation.getCurrentPosition(function(position){init(position)}, // If location is recieve then use it
+                                                 function(){init(default_location)}, // This means it failed, use default
+                                                 geo_options); 
+	} else { 
+	   console.log("Geolocation is not supported by this browser.  Use default."); // this means it's not supported
+	}
+}
+
+function init(position = null) {
+    try{
+        if (position != null) {
+            console.log("setPosition:");
+            console.log("\tUsers Lat: " + ((position && position.coords) ? position.coords.latitude : null));
+            console.log("\tUsers Long: " + ((position && position.coords) ? position.coords.longitude : null));
+            user_position = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        }
+        
+        // if user_position didn't get set, use default_position
+        if (!user_position) {
+            user_position = default_position;
+        }
+        console.log("Positions set, current state is:");
+        console.log("\tUser Location: " + user_position);
+        console.log("\tDefault User Location: " + default_location);
+        
+        mapOptions = {
+            zoom: 15,
+            center: user_position,
+            styles: mapStyles
+        }
+        showHideLoader();
+        //wait two seconds for showHideLoader to complete
+        // this seems wrong that i delay one but not the other.
+        // Feels like I should be passing in a delay for both.  Then it's
+        // obvious to the reader when reading those two calls...er more obvs...
+        setTimeout(function(){ showHideMap() }, 2000);
+    } catch (e) {
+      console.log("Initializing map failed: " + e);
+    }
+
+}
+
+
+/**
+ * Create and fade in the map
  */
 function showHideMap(){
 	$map_out.hide();
